65eb043c3aeb7bfaa3f7f2e8b5508a84594a0c7d
Kam <erkamyaman35@gmail.com>
2024-06-13 23:30:36 +0300

    location-input

--- client/src/Routes/ListPage/ListPage.jsx
+++ client/src/Routes/ListPage/ListPage.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import './ListPage.css'
 export default function ListPage() {
+    const [cityName, setCityName] = useState('London')
     return (
-        <div>ListPage</div>
+        <div className='list-page'>
+            <div className='list-left'>
+                <span>Search results for <b>{cityName}</b> </span>
+                <div className='form-wrapper'>
+                    <form action="">
+
+                        <div className='location-wrapper'>
+                            <label htmlFor="location">Location</label>
+                            <input placeholder='City Location' name='location' type="text" />
+                        </div>
+
+                        <div></div>
+
+                    </form>
+                </div>
+
+            </div>
+            <div className='list-right'>
+            </div>
+        </div>
     )
 }
