a345eb713088dc05935800504efc458e652b213d
Kam <erkamyaman35@gmail.com>
2024-06-13 21:54:26 +0300

    react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,39 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 import HomePage from './Routes/HomePage/HomePage'
-function App() {
+import { createBrowserRouter, RouterProvider, Route, Link, } from "react-router-dom";
+import ListPage from './Routes/ListPage/ListPage';
+import SinglePage from './Routes/SinglePage/SinglePage';
+import LoginPage from './Routes/LoginPage/LoginPage';
 
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element:
+        <HomePage />
+      ,
+    },
+    {
+      path: "/list",
+      element: <ListPage />,
+    },
+    {
+      path: "/single-page",
+      element: <SinglePage />,
+    },
+    {
+      path: "/login-page",
+      element: <LoginPage />,
+    },
+  ]);
   return (
     <div className='app' >
+
       <Navbar />
-      <HomePage />
+      <RouterProvider router={router} />
+
     </div>
   )
 }
