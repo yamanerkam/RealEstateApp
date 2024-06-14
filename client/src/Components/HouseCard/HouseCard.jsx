d745d5213d21bedae187257b4ebad5dfb17b2354
Kam <erkamyaman35@gmail.com>
2024-06-14 20:14:18 +0300

    dummy-data-and-icons

---
+++ client/src/Components/HouseCard/HouseCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function HouseCard() {
+    return (
+        <div className='house-card'>
+            <div className="img-wrapper">
+                <img src="" alt="" />
+            </div>
+            <div className="details-wrapper">
+
+            </div>
+        </div>
+    )
+}
