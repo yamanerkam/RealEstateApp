a4b7ae8a305962e4159f717761ffc0cea4d6f57c
Kam <erkamyaman35@gmail.com>
2024-06-14 20:37:44 +0300

    House-card

--- client/src/Components/HouseCard/HouseCard.jsx
+++ client/src/Components/HouseCard/HouseCard.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 
-export default function HouseCard() {
+export default function HouseCard(props) {
     return (
         <div className='house-card'>
             <div className="img-wrapper">
-                <img src="" alt="" />
+                <img src={props.img} alt="" />
             </div>
             <div className="details-wrapper">
-
+                <h1>{props.title}</h1>
+                <div className='adress'> icon <p>{props.address}</p></div>
+                <p>$ {props.price}</p>
+                <div className='bottom-bar'>
+                    <span className="bedroom">{props.bedroom}</span>
+                    <span className="bathroom">{props.bathroom}</span>
+                </div>
             </div>
         </div>
     )
