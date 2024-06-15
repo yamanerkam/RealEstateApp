0a65538578c0473158a3175a6ed89065ca39b694
Kam <erkamyaman35@gmail.com>
2024-06-15 22:40:43 +0300

    scroolbar-problem

--- client/src/Routes/ListPage/ListPage.jsx
+++ client/src/Routes/ListPage/ListPage.jsx
@@ -36,113 +36,115 @@ export default function ListPage() {
     return (
         <div className='list-page'>
             <div className='list-left'>
-                <span className='list-left-title'>
-                    Search results for <b>{cityName}</b>
-                </span>
-                <div className='form-wrapper'>
-                    <form onSubmit={handleForm}>
-                        <div className='input-wrapper loc'>
-                            <label htmlFor="location">Location</label>
-                            <input
-                                value={formData.location}
-                                onChange={handleFormChange}
-                                placeholder='City Location'
-                                name='location'
-                                type="text"
-                                required
+                <div className="list-left-wrapper">
+                    <span className='list-left-title'>
+                        Search results for <b>{cityName}</b>
+                    </span>
+                    <div className='form-wrapper'>
+                        <form onSubmit={handleForm}>
+                            <div className='input-wrapper loc'>
+                                <label htmlFor="location">Location</label>
+                                <input
+                                    value={formData.location}
+                                    onChange={handleFormChange}
+                                    placeholder='City Location'
+                                    name='location'
+                                    type="text"
+                                    required
+                                />
+                            </div>
+
+                            <div className='second-floor-wrapper'>
+                                <div className='input-wrapper'>
+                                    <label htmlFor="type">Type</label>
+                                    <select
+                                        value={formData.type}
+                                        onChange={handleFormChange}
+                                        id="type"
+                                        name="type"
+                                    >
+                                        <option value="any">any</option>
+                                        <option value="Buy">Buy</option>
+                                        <option value="Rent">Rent</option>
+                                    </select>
+                                </div>
+
+                                <div className='input-wrapper'>
+                                    <label htmlFor="property">Property</label>
+                                    <select
+                                        id="property"
+                                        value={formData.property}
+                                        onChange={handleFormChange}
+                                        name="property"
+                                    >
+                                        <option value="any">any</option>
+                                        <option value="Apartment">Apartment</option>
+                                        <option value="House">House</option>
+                                        <option value="Condo">Condo</option>
+                                        <option value="Land">Land</option>
+                                    </select>
+                                </div>
+
+                                <div className='input-wrapper'>
+                                    <label htmlFor="min">Min Price</label>
+                                    <input
+                                        value={formData.min}
+                                        onChange={handleFormChange}
+                                        placeholder='any'
+                                        min="0"
+                                        name='min'
+                                        type="number"
+                                    />
+                                </div>
+
+                                <div className='input-wrapper'>
+                                    <label htmlFor="max">Max Price</label>
+                                    <input
+                                        value={formData.max}
+                                        onChange={handleFormChange}
+                                        placeholder='any'
+                                        min="0"
+                                        name='max'
+                                        type="number"
+                                    />
+                                </div>
+
+                                <div className='input-wrapper'>
+                                    <label htmlFor="bedroom">Bedroom</label>
+                                    <input
+                                        value={formData.bedroom}
+                                        onChange={handleFormChange}
+                                        placeholder='any'
+                                        min="0"
+                                        name='bedroom'
+                                        type="number"
+                                    />
+                                </div>
+                            </div>
+
+                            <button type='submit'><CiSearch size={32} /></button>
+                        </form>
+                    </div>
+
+                    <div className="items">
+
+                        {listData && listData.map((item) => (
+                            <HouseCard
+                                key={item.id}
+                                img={item.img}
+                                address={item.address}
+                                title={item.title}
+                                price={item.price}
+                                bedroom={item.bedroom}
+                                bathroom={item.bathroom}
+
                             />
-                        </div>
+                        ))}
 
-                        <div className='second-floor-wrapper'>
-                            <div className='input-wrapper'>
-                                <label htmlFor="type">Type</label>
-                                <select
-                                    value={formData.type}
-                                    onChange={handleFormChange}
-                                    id="type"
-                                    name="type"
-                                >
-                                    <option value="any">any</option>
-                                    <option value="Buy">Buy</option>
-                                    <option value="Rent">Rent</option>
-                                </select>
-                            </div>
+                    </div>
 
-                            <div className='input-wrapper'>
-                                <label htmlFor="property">Property</label>
-                                <select
-                                    id="property"
-                                    value={formData.property}
-                                    onChange={handleFormChange}
-                                    name="property"
-                                >
-                                    <option value="any">any</option>
-                                    <option value="Apartment">Apartment</option>
-                                    <option value="House">House</option>
-                                    <option value="Condo">Condo</option>
-                                    <option value="Land">Land</option>
-                                </select>
-                            </div>
-
-                            <div className='input-wrapper'>
-                                <label htmlFor="min">Min Price</label>
-                                <input
-                                    value={formData.min}
-                                    onChange={handleFormChange}
-                                    placeholder='any'
-                                    min="0"
-                                    name='min'
-                                    type="number"
-                                />
-                            </div>
-
-                            <div className='input-wrapper'>
-                                <label htmlFor="max">Max Price</label>
-                                <input
-                                    value={formData.max}
-                                    onChange={handleFormChange}
-                                    placeholder='any'
-                                    min="0"
-                                    name='max'
-                                    type="number"
-                                />
-                            </div>
-
-                            <div className='input-wrapper'>
-                                <label htmlFor="bedroom">Bedroom</label>
-                                <input
-                                    value={formData.bedroom}
-                                    onChange={handleFormChange}
-                                    placeholder='any'
-                                    min="0"
-                                    name='bedroom'
-                                    type="number"
-                                />
-                            </div>
-                        </div>
-
-                        <button type='submit'><CiSearch size={32} /></button>
-                    </form>
-                </div>
-
-                <div className="items">
-
-                    {listData && listData.map((item) => (
-                        <HouseCard
-                            key={item.id}
-                            img={item.img}
-                            address={item.address}
-                            title={item.title}
-                            price={item.price}
-                            bedroom={item.bedroom}
-                            bathroom={item.bathroom}
-
-                        />
-                    ))}
 
                 </div>
-
-
             </div>
             <div className='list-right'>
                 <Map />
