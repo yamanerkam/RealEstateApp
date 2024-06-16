ea9b78a641243c8ea1a67d6bddb7265ee54c7ff7
Kam <erkamyaman35@gmail.com>
2024-06-16 16:38:35 +0300

    mid-floor-done

--- client/src/Routes/SinglePage/SinglePage.jsx
+++ client/src/Routes/SinglePage/SinglePage.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import './SinglePage.css'
+import { singlePostData } from '../../Lib/dummydata.js'
+import { IoLocationOutline } from "react-icons/io5";
+import catPP from '../../../src/assets/cat.webp'
+
 export default function SinglePage() {
     return (
         <div className='single-page'>
-            <div className='sp-details'>left</div>
+            <div className='sp-details'>
+                <div className='images'></div>
+                <div className='mid-floor'>
+                    <div className='mf-left'>
+                        <h1>{singlePostData.title}</h1>
+                        <span className='address'> <IoLocationOutline className='loc-icon' size={22} />
+                            <p>{singlePostData.address}</p></span>
+                        <p className='price'>$ {singlePostData.price}</p>
+                    </div>
+
+                    <div className='wrp'>
+                        <img src={catPP} alt="" />
+                        <p className="userName">Erkam Yaman</p>
+                    </div>
+                </div>
+
+            </div>
             <div className="sp-features">right</div>
-        </div>
+        </div >
     )
 }
