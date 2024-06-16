df3c653ef34e53a35cdf7836a7f2aa34603e3ca7
Kam <erkamyaman35@gmail.com>
2024-06-16 18:11:49 +0300

    Margin-to-scoll-bar

--- client/src/Routes/SinglePage/SinglePage.jsx
+++ client/src/Routes/SinglePage/SinglePage.jsx
@@ -7,32 +7,34 @@ import catPP from '../../../src/assets/cat.webp'
 export default function SinglePage() {
     return (
         <div className='single-page'>
-            <div className='sp-details'>
-                <div className='images'>
-                    <div className='img-left'>
-                        <img className='bigIMG' src={singlePostData.images[0]} alt="" />
+            <div className='spd-wrapper'>
+                <div className='sp-details'>
+                    <div className='images'>
+                        <div className='img-left'>
+                            <img className='bigIMG' src={singlePostData.images[0]} alt="" />
+                        </div>
+                        <div className='img-right'>                        <img className='smallIMG' src={singlePostData.images[1]} alt="" />                        <img className='smallIMG' src={singlePostData.images[2]} alt="" />                        <img className='smallIMG' src={singlePostData.images[3]} alt="" />
+                        </div>
                     </div>
-                    <div className='img-right'>                        <img className='smallIMG' src={singlePostData.images[1]} alt="" />                        <img className='smallIMG' src={singlePostData.images[2]} alt="" />                        <img className='smallIMG' src={singlePostData.images[3]} alt="" />
-                    </div>
-                </div>
-                <div className='mid-floor'>
-                    <div className='mf-left'>
-                        <h1>{singlePostData.title}</h1>
-                        <span className='address'> <IoLocationOutline className='loc-icon' size={22} />
-                            <p>{singlePostData.address}</p></span>
-                        <p className='price'>$ {singlePostData.price}</p>
+                    <div className='mid-floor'>
+                        <div className='mf-left'>
+                            <h1>{singlePostData.title}</h1>
+                            <span className='address'> <IoLocationOutline className='loc-icon' size={22} />
+                                <p>{singlePostData.address}</p></span>
+                            <p className='price'>$ {singlePostData.price}</p>
+                        </div>
+
+                        <div className='mf-right'>
+                            <img src={catPP} alt="" />
+                            <p className="userName">Erkam Yaman</p>
+                        </div>
                     </div>
 
-                    <div className='mf-right'>
-                        <img src={catPP} alt="" />
-                        <p className="userName">Erkam Yaman</p>
+                    <div className='sp-d-bottom'>
+                        <p>{singlePostData.description}</p>
                     </div>
-                </div>
 
-                <div className='sp-d-bottom'>
-                    <p>{singlePostData.description}</p>
                 </div>
-
             </div>
             <div className="sp-features">right</div>
         </div >
