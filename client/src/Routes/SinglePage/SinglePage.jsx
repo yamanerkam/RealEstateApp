33f686cc92bf41c7e56f5b0a3b0289da490e1691
Kam <erkamyaman35@gmail.com>
2024-06-17 17:31:31 +0300

    single-page-right-side

--- client/src/Routes/SinglePage/SinglePage.jsx
+++ client/src/Routes/SinglePage/SinglePage.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 import './SinglePage.css'
 import { singlePostData } from '../../Lib/dummydata.js'
 import { IoLocationOutline } from "react-icons/io5";
+import { PiPawPrintThin } from "react-icons/pi";
+import { PiMoneyThin } from "react-icons/pi";
+import { SlSizeFullscreen } from "react-icons/sl";
+import { IoBedOutline } from "react-icons/io5";
+import { FaBath } from "react-icons/fa";
+import { LuSchool2 } from "react-icons/lu";
+import { PiBusThin } from "react-icons/pi";
+import { IoRestaurantOutline } from "react-icons/io5";
+import { CiChat1 } from "react-icons/ci";
+import { CiBookmark } from "react-icons/ci";
+import { VscTools } from "react-icons/vsc";
+
+
 import catPP from '../../../src/assets/cat.webp'
 
 export default function SinglePage() {
@@ -36,7 +49,87 @@ export default function SinglePage() {
 
                 </div>
             </div>
-            <div className="sp-features">right</div>
+            <div className="sp-features">
+                <div className="spf-wrapper">
+                    <p className="title">General</p>
+                    <div className="listVertical">
+                        <div className="feature">
+                            <VscTools size={22} className='ft-icon' />
+                            <div className="featureText">
+                                <span>Utilities</span>
+                                <p>Renter is responsible</p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <PiPawPrintThin size={22} className='ft-icon' />
+                            <div className="featureText">
+                                <span>Pet Policy</span>
+                                <p>Pets Allowed</p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <PiMoneyThin size={22} className='ft-icon' />
+                            <div className="featureText">
+                                <span>Property Fees</span>
+                                <p>Must have 3x the rent in total household income</p>
+                            </div>
+                        </div>
+                    </div>
+                    <p className="title">Sizes</p>
+                    <div className="sizes">
+                        <div className="size">
+                            <SlSizeFullscreen size={22} className='ft-icon-s' />
+                            <span>80 sqft</span>
+                        </div>
+                        <div className="size">
+                            <IoBedOutline size={22} className='ft-icon-s' />
+                            <span>2 beds</span>
+                        </div>
+                        <div className="size">
+                            <FaBath size={22} className='ft-icon-s' />
+                            <span>1 bathroom</span>
+                        </div>
+                    </div>
+                    <p className="title">Nearby Places</p>
+                    <div className="listHorizontal">
+                        <div className="feature">
+                            <LuSchool2 size={22} className='ft-icon' />
+                            <div className="featureText">
+                                <span>School</span>
+                                <p>250m away</p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <PiBusThin size={22} className='ft-icon' />
+                            <div className="featureText">
+                                <span>Bus Stop</span>
+                                <p>100m away</p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <IoRestaurantOutline size={22} className='ft-icon' />
+                            <div className="featureText">
+                                <span>Restaurant</span>
+                                <p>200m away</p>
+                            </div>
+                        </div>
+                    </div>
+                    <p className="title">Location</p>
+                    <div className="mapContainer">
+                    </div>
+                    <div className="buttons">
+                        <button>
+                            <CiChat1 size={22} />
+                            Send a Message
+                        </button>
+                        <button>
+                            <CiBookmark size={22} />
+                            Save the Place
+                        </button>
+                    </div>
+                </div>
+            </div >
         </div >
+
     )
 }
