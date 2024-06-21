a7f3d97a4ed089ee54570aa560d739909f4ec291
Kam <erkamyaman35@gmail.com>
2024-06-21 21:53:13 +0300

    userRoute

--- api/app.js
+++ api/app.js
@@ -1,7 +1,9 @@
 import express from 'express'
-
+import userRoute from './routes/user.route.js'
 const app = express()
 const PORT = 3001
+
+app.use('/api/u', userRoute)
 
 app.use('/api/test', (req, res) => {
     res.send('it works')
---
+++ api/routes/user.route.js
@@ -0,0 +1,17 @@
+import express from "express";
+const router = express.Router()
+router.use(express.json());
+
+router.get('/', (req, res) => {
+    res.send('user-route')
+})
+
+router.get('/user', (req, res) => {
+    res.send('user')
+})
+
+router.get('/user/:id', (req, res) => {
+    res.send(':id')
+})
+
+export default router
