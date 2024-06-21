7243d9adc1aba751ee1a0c5c8618db21cf3573fe
Kam <erkamyaman35@gmail.com>
2024-06-21 23:33:16 +0300

    hashed-password

--- api/app.js
+++ api/app.js
@@ -4,6 +4,8 @@ import authRoute from './routes/auth.route.js'
 const app = express()
 const PORT = 3001
 
+app.use(express.json())
+
 app.use('/api/auth', authRoute)
 
 app.use('/api/test', (req, res) => {
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,10 @@
-export const register = (req, res) => {
+import bcrypt from 'bcrypt'
+export const register = async (req, res) => {
+    const { username, email, password } = req.body
+    const hashedPassword = await bcrypt.hash(password, 10)
     res.send('register contoller')
+    console.log(hashedPassword)
+
 }
 
 export const login = (req, res) => {
