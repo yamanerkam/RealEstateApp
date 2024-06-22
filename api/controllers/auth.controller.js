2d654130065b3f223c2bcdd678c0f9ea43ce3a13
Kam <erkamyaman35@gmail.com>
2024-06-22 23:57:38 +0300

    prisma-login

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,20 +3,44 @@ import prisma from '../lib/prisma.js'
 export const register = async (req, res) => {
     const { username, email, password } = req.body
     const hashedPassword = await bcrypt.hash(password, 10)
-    res.send('register contoller')
-    const newUser = await prisma.user.create({
-        data: {
-            username,
-            email,
-            password: hashedPassword,
-        }
-    })
-    console.log(newUser)
+    try {
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hashedPassword,
+            }
+        })
+        res.status(201).json({ message: 'user created succesfully' })
+        console.log(newUser)
+    } catch (error) {
+        console.log(error)
+        res.status(501)
+    }
+
 
 }
 
-export const login = (req, res) => {
-    res.send('login contoller')
+export const login = async (req, res) => {
+    const { password, username } = req.body
+    try {
+        const user = await prisma.user.findUnique({
+            where: { username }
+        })
+
+        if (!user) return res.status(401).json({ message: 'user not found!' })
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+
+        if (!isPasswordCorrect) res.status(401).json({ message: 'wrong password!' })
+
+        res.setHeader("Set-Cookie", "test=" + "biscuit").json({ message: 'success' })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: 'failed to login' })
+    }
+
 }
 
 export const logout = (req, res) => {
