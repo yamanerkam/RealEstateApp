fa81d62c849afea1648da21a45d92dba6e7ad4e5
Kam <erkamyaman35@gmail.com>
2024-06-24 14:46:31 +0300

    useNavigate

--- client/src/Routes/RegisterPage/Register.jsx
+++ client/src/Routes/RegisterPage/Register.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
 import './Register.css'
+import { useNavigate } from 'react-router-dom';
+
 export default function Register() {
+    const navigate = useNavigate();
+
     const [formData, setFormData] = useState({
         email: '',
         username: '',
@@ -23,10 +27,13 @@ export default function Register() {
     return (
         <div className='register-page'>
             <form onSubmit={handleSubmit}>
+                <h1>Create an Account</h1>
                 <input required name='email' value={formData.email} onChange={handleChange} placeholder='Email' type="text" />
                 <input required name='username' value={formData.username} onChange={handleChange} placeholder='Username' type="text" />
                 <input required name='password' value={formData.password} onChange={handleChange} placeholder='Password' type="password" />
                 <button type='submit'>Register</button>
+                <span onClick={() => navigate('/login')}>Already have an account</span>
+
             </form>
         </div>
     )
