a3665ebd37e983909d225628ba8b4598f7b8a4e8
Kam <erkamyaman35@gmail.com>
2024-06-26 17:43:25 +0300

    token-verify

--- api/controllers/test.controller.js
+++ api/controllers/test.controller.js
@@ -7,11 +7,13 @@ dotenv.config()
 
 export const admin = async (req, res) => {
     const token = req.cookies.token
-    if (!token) return res.status(401).json({ message: 'no user logged in' })
+    if (!token) return res.status(401).json({ message: 'not Authenticated' })
     console.log(token)
 
-    console.log('admin is succesful')
-    res.send('admin')
+    jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+        if (err) return res.status(403).json({ message: 'Token is not valid!' })
+    })
+    res.status(200).json("admin is authenticated")
 }
 
 export const loggedIn = async (req, res) => {
