094515bb5ff8aaea208e26f0b6352f8345acf764
Kam <erkamyaman35@gmail.com>
2024-07-09 15:37:34 +0300

    deleteUsers

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -6,31 +6,47 @@ export const getPosts = async (req, res) => {
         res.send(posts)
     } catch (error) {
         console.log(error)
-        res.status(500).json({ msg: 'failed to get users ' })
+        res.status(500).json({ msg: 'failed to get the posts ' })
     }
 }
 
 export const getPost = async (req, res) => {
+    const id = req.params.id
     try {
-        const posts = await prisma.post.findMany()
-        res.send(posts)
+        const post = await prisma.post.findUnique({
+            where: { id },
+        })
+        res.status(200).json({ post })
     } catch (error) {
         console.log(error)
-        res.status(500).json({ msg: 'failed to get users ' })
+        res.status(500).json({ msg: 'failed to get the post ' })
     }
 }
 
 export const addPost = async (req, res) => {
+    const body = req.body
+    const tokenUserId = req.userId;
+
     try {
-        const posts = await prisma.post.findMany()
-        res.send(posts)
+        const newPost = await prisma.post.create({
+            data: {
+                ...body,
+                userId: tokenUserId,
+            }
+        })
+        res.send(newPost)
     } catch (error) {
         console.log(error)
-        res.status(500).json({ msg: 'failed to get users ' })
+        res.status(500).json({ msg: 'failed to add a user ' })
     }
 }
 
 export const updatePost = async (req, res) => {
+    const id = req.params.id
+    const tokenUserId = req.userId;
+    if (id !== tokenUserId) {
+        return res.status(403).json({ message: "Not Authorized!" });
+    }
     try {
         const posts = await prisma.post.findMany()
         res.send(posts)
@@ -41,11 +57,23 @@ export const updatePost = async (req, res) => {
 }
 
 export const deletePost = async (req, res) => {
+    const id = req.params.id
+    const tokenUserId = req.userId;
+
     try {
-        const posts = await prisma.post.findMany()
-        res.send(posts)
+        const post = await prisma.post.findUnique({
+            where: { id },
+        })
+        if (post.userId !== tokenUserId) {
+            return res.status(403).json({ message: "Not Authorized!" });
+
+        }
+        await prisma.post.delete({
+            where: { id },
+        })
+        res.status(200).json({ msg: "post deleted" })
     } catch (error) {
         console.log(error)
-        res.status(500).json({ msg: 'failed to get users ' })
+        res.status(500).json({ message: "Failed to delete post" });
     }
 }
